e91567068bd38872c41982e23b7116fb9beef2b2
Kai Mies <develop@regnavi.de>
2023-10-31 06:59:38 +0100

    Add dev shell

--- flake.nix
+++ flake.nix
@@ -1,11 +1,23 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
-templates = {
-    python = {
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    templates = {
+      python = {
         path = ./python;
+      };
     };
-};
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        alejandra
+        nixd
+      ];
+    };
   };
 }
