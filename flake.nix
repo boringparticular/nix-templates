a4578b3526b85d069791c571234b7c15070c2949
Kai Mies <develop@regnavi.de>
2023-11-25 18:20:42 +0100

    Add default plugins

--- flake.nix
+++ flake.nix
@@ -1,21 +1,29 @@
 {
   description = "A very basic flake";
 
+  inputs = {
+    official-templates.url = github:NixOS/templates;
+  };
+
   outputs = {
     self,
     nixpkgs,
+    official-templates,
+    ...
   }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
   in {
-    templates = {
-      python = {
-        path = ./python;
-      };
-      go = {
-        path = ./go;
-      };
-    };
+    templates =
+      {
+        python = {
+          path = ./python;
+        };
+        go = {
+          path = ./go;
+        };
+      }
+      // official-templates.templates;
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
         alejandra
